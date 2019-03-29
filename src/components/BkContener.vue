71191856a9434903e39978425ff6f125a10b7035
Free Archer <bespalov.alexey@yandex.ru>
2019-03-29 09:09:03 +0700

    first commit

---
+++ src/components/BkContener.vue
@@ -0,0 +1,21 @@
+<template>
+  <p> First Cpmponent</p>
+</template>
+
+<script>
+import BkItem from './BkItem.vue'
+
+  export default {
+    components: {
+    BkItem
+  }, 
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+.BkItem {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/BkItem.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+
+    data: () => ({
+    })
+  }
+</script>
+
+<style>
+.card {
+  background-color: aqua;
+  overflow: hidden;
+  min-height: 80px;
+  min-width: 80px;
+
+
+}
+.bk-container {
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+    flex-wrap: wrap;
+
+}
+</style>
