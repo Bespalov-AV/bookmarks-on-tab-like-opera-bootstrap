29cc9f7916e464a605c7ed6b1b8dc33dfd3abde3
Free Archer <bespalov.alexey@yandex.ru>
2019-04-13 12:17:22 +0700

    Заработал рекурсивный вызов компанентов!

--- src/components/BkContener.vue
+++ src/components/BkContener.vue
@@ -1,91 +1,81 @@
 <template>
-<div>
-    <div 
-      class="title" 
+  <div>
+    <div
+      class="title"
       v-on:click="setOpenFolder($event)"
       v-click-outside="onClickOutside"
-    >
-      {{ title }}
-    </div>
-    <BkOpenFolder 
+    >{{ title }}</div>
+    <BkOpenFolder
       v-if="openFolder"
       v-on:click="setOpenFolder($event)"
-      :bkChildren = bkFolder
-      :title = title
-    />    
-  <div class="bk-contener">
-    <div v-for="currentBk of bkFolder"
-        :key="currentBk.id"
-        >
-        <BkFolder v-if="currentBk.children"
-          :bkChildren = currentBk.children
-          :title = currentBk.title 
-          :index = currentBk.index 
-          :isOpenFolder = openFolder 
-        >
-        </BkFolder>
+      :bkChildren="bkFolder"
+      :title="title"
+    />
+    <div class="bk-contener">
+      <div v-for="currentBk of bkFolder" :key="currentBk.id">
+        <BkFolder
+          v-if="currentBk.children"
+          :bkChildren="currentBk.children"
+          :title="currentBk.title"
+          :index="currentBk.index"
+          :isOpenFolder="openFolder"
+        ></BkFolder>
 
-        <BkItem v-else
-          :currentBk = currentBk
-          :isOpenFolder = openFolder 
-        >
-        </BkItem> 
+        <BkItem v-else :currentBk="currentBk" :isOpenFolder="openFolder"></BkItem>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import BkItem from './BkItem.vue'
-import BkFolder from './BkFolder.vue'
-import BkOpenFolder from './BkOpenFolder.vue'
+import BkItem from "./BkItem.vue";
+import BkFolder from "./BkFolder.vue";
+import BkOpenFolder from "./BkOpenFolder.vue";
 
- 
-
-  export default {
-    name: "BkContener",
-    props:  ['bkFolder', 'title'],
-    components: {BkItem, BkFolder, BkOpenFolder},
-    data: () => ({
-      openFolder: false
-    }),
-    methods: {
-      log(param) {
-        console.log(param)
-      },
-      setOpenFolder(event) {
-        this.openFolder = !this.openFolder
-        console.log(this.openFolder)
-      },
-      onClickOutside (e, el) {
-        if (e.target != el) {
-          this.openFolder = false
-        }
-      }      
+export default {
+  name: "BkContener",
+  props: ["bkFolder", "title"],
+  components: { BkItem, BkFolder, BkOpenFolder },
+  data: () => ({
+    openFolder: false
+  }),
+  methods: {
+    log(param) {
+      console.log(param);
     },
-    beforeCreate: function () {
-  //this.$options.components.TreeFolderContents = require('./tree-folder-contents.vue').default
-}
+    setOpenFolder(event) {
+      this.openFolder = !this.openFolder;
+      console.log(this.openFolder);
+    },
+    onClickOutside(e, el) {
+      if (e.target != el) {
+        this.openFolder = false;
+      }
+    }
+  },
+  beforeCreate: function() {
+    //this.$options.components.TreeFolderContents = require('./tree-folder-contents.vue').default
   }
+};
 </script>
 
 <style>
 .bk-contener {
-    display: flex;
-    flex-wrap: wrap;
-    /* border: 1px solid #333; */
-    margin-top: 5px;
-    margin-bottom: 5px;
-    margin-left: 5px;
-    margin-right: 5px;
-    width: 200px;
-    height: 140;
-    overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  /* border: 1px solid #333; */
+  margin-top: 5px;
+  margin-bottom: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  width: 200px;
+  height: 140;
+  overflow: hidden;
 }
 .title {
-    display: block;
-    cursor: pointer;
-    width: 100%;
-    text-align: center;
+  display: block;
+  cursor: pointer;
+  width: 100%;
+  text-align: center;
 }
 </style>
--- src/components/BkFolder.vue
+++ src/components/BkFolder.vue
@@ -1,66 +1,55 @@
 <template>
-
   <div v-click-outside="onClickOutside">
+    <div v-if="index < 4 || isOpenFolder" v-on:click="setOpenFolder" class="bk-folder">{{ title }}</div>
 
-  <div v-if="index < 4 || isOpenFolder"
-    v-on:click="setOpenFolder"
-    class="bk-folder">
-    {{ title }}
+    <BkOpenFolder v-if="openFolder" v-on:click="setOpenFolder($event)" :bkChildren="bkChildren"/>
   </div>
-
-  <BkOpenFolder 
-    v-if="openFolder"
-    v-on:click="setOpenFolder($event)"
-    :bkChildren = bkChildren
-  />
-
-  </div>
-
 </template>
 
 <script>
-import BkOpenFolder from './BkOpenFolder.vue'
+import BkOpenFolder from "./BkOpenFolder.vue";
 
-  export default {
-    name: "BkFolder",
-    props:  ['bkChildren', 'title', 'index', 'isOpenFolder'],
-    components: {BkOpenFolder}, 
+export default {
+  name: "BkFolder",
+  props: ["bkChildren", "title", "index", "isOpenFolder"],
+  components: { BkOpenFolder },
+  //components: { BkOpenFolder: () => import("./BkOpenFolder.vue") },
 
-    data: () => ({
-      openFolder: false,
-      //isThisOpenFolder: false
-    }),
-    methods: {
-      setOpenFolder(event) {
-        this.openFolder = !this.openFolder
-        //this.isThisOpenFolder = true
-      },
-      onClickOutside (e, el) {
-        if (e.target != el) {
-          this.openFolder = false
-          //this.isThisOpenFolder = false
-        }
-      }
+  data: () => ({
+    openFolder: false
+    //isThisOpenFolder: false
+  }),
+  methods: {
+    setOpenFolder(event) {
+      this.openFolder = !this.openFolder;
+      //this.isThisOpenFolder = true
+    },
+    onClickOutside(e, el) {
+      // if (e.target != el) {
+      //   this.openFolder = false;
+      //this.isThisOpenFolder = false
+      //}
     }
   }
+};
 </script>
 
 <style>
 .bk-folder {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 90px;
-    height: var(--heght-bk); 
-    border: 1px solid #216BA3;
-    border-radius: 5px;
-    /* box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);     */
-    margin: 2px;
-    background: #888;
-    overflow: hidden;
-    cursor: pointer;
-    color: white;
-    font-size: 15px;
-    /* vertical-align: middle; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90px;
+  height: var(--heght-bk);
+  border: 1px solid #216ba3;
+  border-radius: 5px;
+  /* box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);     */
+  margin: 2px;
+  background: #888;
+  overflow: hidden;
+  cursor: pointer;
+  color: white;
+  font-size: 15px;
+  /* vertical-align: middle; */
 }
 </style>
