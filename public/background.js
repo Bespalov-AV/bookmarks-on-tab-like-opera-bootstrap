2c6ed49abb18b235ab8a935e5255bccb54971034
Free Archer <bespalov.alexey@yandex.ru>
2019-04-22 09:10:05 +0700

    правки

--- public/background.js
+++ public/background.js
@@ -1,9 +1,3 @@
-// chrome.tabs.onCreated.addListener(() => {
-//     console.log('new tab')
-// })
-
-
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({ url: chrome.extension.getURL("index.html") });
-    console.log('!!!!')
 });
